e62640ebbc5388d7ec4283ef405a089159440b10
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-05-20 17:36:44 +0200

    Edited project's name

--- pac_mon/src/pac_mon/Demo.java
+++ pac_mon/src/pac_mon/Demo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pak_emon;
+package pac_mon;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
--- pac_mon/src/pac_mon/PaC_MoN.java
+++ pac_mon/src/pac_mon/PaC_MoN.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pak_emon;
+package pac_mon;
 
 /**
  * PaC'MoN Project
