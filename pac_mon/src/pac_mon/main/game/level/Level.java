24eaaccf805edaaa42a7013569115c7fc62883b4
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-05-21 14:20:17 +0200

    Modified Level Class

---
+++ pac_mon/src/pac_mon/main/game/level/Level.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pac_mon.main.game.level;
+
+import java.util.ArrayList;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import pac_mon.main.Component;
+import pac_mon.main.game.level.items.Item;
+import static pac_mon.main.game.level.items.Item.Items.*;
+import static pac_mon.main.graphics.Calcul.*;
+
+/**
+ * Niveau
+ * @author Skyde
+ */
+public class Level {
+    
+    private ArrayList<Item> items; // liste des items du niveau
+    
+    /**
+     * Constructeur du niveau
+     */
+    public Level() {
+        this.items = new ArrayList<>();
+        
+        generate(); // on genere le niveau
+    }
+    
+    /**
+     * Generation du niveau
+     */
+    public void generate() {
+        
+        /* Test de génération d'items en mouvement */
+        /*
+        int width = Component.width*Component.scale-88;
+        int height = Component.height*Component.scale-149;
+        int size;
+        for (int i=0; i<10000; i++) {
+            size = round(random(5,15));
+            items.add(new Item(round(random(0, width)), round(random(0, height)), random(-1,1), random(-1,1), size));
+        }
+        */
+        
+        items.add(new Item(25, 25, DOT));
+        items.add(new Item(60, 30, FRUIT));
+        items.add(new Item(40, 50, POWER));
+        
+    }
+    
+    /**
+     * Initialisation du niveau
+     */
+    public void init() {
+        
+    }
+    
+    /**
+     * Mise a jour du niveau
+     */
+    public void update() {
+        
+    }
+    
+    /**
+     * Affiche le rendu du niveau
+     */
+    public void render() {
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // met a jour le background
+        
+        for (Item item : items) {
+            item.render(); // on fait le rendu des tiles
+        }
+    }
+}
