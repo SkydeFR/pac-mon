709ab5e5998a9e5d50386436115545bc0b9ec7e8
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-05-21 02:20:53 +0200

    Minor changes

---
+++ pac_mon/src/pac_mon/main/Pac_mon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pac_mon.main;
+
+/**
+ * PaC'MoN Project
+ * @author Skyde
+ */
+public class Pac_mon {
+     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Component game = new Component();
+        game.run();
+    }
+    
+}
